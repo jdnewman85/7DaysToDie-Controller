2b26ac63646fbcb050e798c965ddd12fca2290f9
James Newman <jdnewman85@gmail.com>
2015-05-28 01:05:12 -0500

    Switched from states, to triggers

--- main.go
+++ main.go
@@ -4,32 +4,42 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"regexp"
 	"strings"
 )
 
 type stateFunc func()
 
+type triggerFunc func(map[string]string)
+
+type trigger struct {
+	reString string
+	re *regexp.Regexp
+	callback triggerFunc
+}
+
 var (
 	serverConn net.Conn
 	serverBuffer *bufio.Reader
 
-
-	state string = "state_password"
-	stateMap = map[string]stateFunc {
-		"state_password": state_password,
-		"state_login": state_login,
-		"state_run": state_run,
-		"state_debug": state_debug,
+	triggers = []trigger{
+		trigger{"^Please enter password:$", nil, password_trigger},
+		trigger{"^Logon successful.$", nil, login_trigger},
+		trigger{".*blargy.*", nil, blargy_trigger},
+		trigger{".*SendMeHome.*", nil, home_trigger},
 	}
-
-	line string
-	err error
 )
 
 func main() {
 	fmt.Println("BEGIN")
 
+	//Compile trigger regexps
+	for i := range triggers {
+		triggers[i].re = regexp.MustCompile(triggers[i].reString) //OPT Method?
+	}
+
 	//Server Connect
+	var err error
 	serverConn, err = net.Dial("tcp", "7days.prototekokc.com:8081")
 	if err != nil {
 		panic(err)
@@ -38,10 +48,9 @@ func main() {
 
 	serverBuffer := bufio.NewReader(serverConn)
 
-
 	//Read those lines
 	fmt.Println("Waiting for password")
-	for line, err = serverBuffer.ReadString('\n'); err == nil; line, err = serverBuffer.ReadString('\n') {
+	for line, err := serverBuffer.ReadString('\n'); err == nil; line, err = serverBuffer.ReadString('\n') {
 
 		//Remove extra whitespace and skip empty lines
 		line := strings.TrimSpace(line)
@@ -49,8 +58,21 @@ func main() {
 			continue
 		}
 
-		//Main State Machine
-		stateMap[state]()
+		//Check for triggers
+		//OPT Function, or something?
+		for i := range triggers {
+			matches := triggers[i].re.FindStringSubmatch(line)
+			if matches != nil {
+				//Assemble a matches map
+				matchMap := make(map[string]string)
+				matchNames := triggers[i].re.SubexpNames()
+				for j, match := range matches {
+					matchMap[matchNames[j]] = match
+				}
+				//Run the trigger
+				triggers[i].callback(matchMap)
+			}
+		}
 
 
 		fmt.Printf("SERVER: '%s'\n", line)
@@ -60,33 +82,22 @@ func main() {
 	fmt.Println("END")
 }
 
-func state_password() {
-	if strings.Contains(line, "Please enter password:") {
-		fmt.Fprintf(serverConn, "%s\n", "TrumpTikAdmin%")
-		fmt.Println("Waiting for logon confirm.")
-		state = "state_login"
-	}
+func password_trigger(reMatchMap map[string]string) {
+	fmt.Fprintf(serverConn, "%s\n", "TrumpTikAdmin%")
+	fmt.Println("Waiting for logon confirm.")
 }
 
-func state_login() {
-	if strings.Contains(line, "Logon successful.") {
-		fmt.Println("Waiting for commands.")
-		state = "state_run"
-	}
+func login_trigger(reMatchMap map[string]string) {
+	fmt.Println("Waiting for commands.")
 }
 
-func state_run() {
-	switch {
-	case strings.Contains(line, "blargy"):
-		fmt.Println("blargy command!")
-		fmt.Fprintf(serverConn, "say \"%s\"\n", "Yay blargs!")
-	case strings.Contains(line, "SendMeHome"):
-		fmt.Println("Home command!")
-		fmt.Fprintf(serverConn, "%s\n", "tele scienthsine 2956 82 1220")
-	}
+func blargy_trigger(reMatchMap map[string]string) {
+	fmt.Println("blargy command!")
+	fmt.Fprintf(serverConn, "say \"%s\"\n", "Yay blargs!")
 }
 
-func state_debug() {
-
+func home_trigger(reMatchMap map[string]string) {
+	fmt.Println("Home command!")
+	fmt.Fprintf(serverConn, "%s\n", "tele scienthsine 2956 82 1220")
 }
 

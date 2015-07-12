1a3ed52b4c23813609458eb8241bcb8a242f9905
James Newman <jdnewman85@gmail.com>
2015-07-12 15:17:39 -0500

    Updae

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
+	"math/rand"
 	"net"
 	"regexp"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type stateFunc func()
@@ -16,6 +20,35 @@ type trigger struct {
 	reString string
 	re *regexp.Regexp
 	callback triggerFunc
+}
+
+type Player struct {
+	id string
+	name string
+	steamId string
+
+	lastUpdate int64
+	online bool
+	remote bool
+	ip string
+	ping uint64
+
+	x, y, z float64
+	u, v, w float64
+
+	//TEMP OPT Stop using 64 bits, and do the conversions where needed
+	health uint64
+	deaths uint64
+	zKills uint64
+	pKills uint64
+	score uint64
+	level uint64
+}
+
+type Point struct {
+	x float64
+	y float64
+	z float64
 }
 
 const (
@@ -106,6 +139,23 @@ var (
 	}
 )
 
+var (
+	//NOTE: Storing a reference from name, steamid, and id in same map
+	playerMap map[string]*Player = make(map[string]*Player)
+	milliseconds int64
+)
+
+var (
+	spawnPoints []Point
+	mainBaseHorde bool
+	zombieNum int64
+	zombies = []int{7, 11, 12,
+			1, 3, 4, 5, 13, 14, 15, 16, 17,
+			19, 8, 9, 10,
+			20, 21, 18, 18, 2, 2,
+		}
+)
+
 func main() {
 	fmt.Println("BEGIN")
 	//fmt.Printf("\n\n\n%s\n\n\n", player_regex)
@@ -124,6 +174,10 @@ func main() {
 	defer serverConn.Close()
 
 	serverBuffer := bufio.NewReader(serverConn)
+
+	//TEMP
+	go updatePlayerInfo_thread()
+	go mainBaseHorde_thread()
 
 	//Read those lines
 	fmt.Println("Waiting for password")
@@ -152,7 +206,7 @@ func main() {
 		}
 
 
-		fmt.Printf("SERVER: '%s'\n", line)
+		//fmt.Printf("SERVER: '%s'\n", line)
 	}
 
 
@@ -173,14 +227,136 @@ func command_trigger(reMatchMap map[string]string) {
 	switch reMatchMap["command"] {
 	case "pm lp":
 		fmt.Fprintf(serverConn, "%s\n", "lp")
-	case "pm storeWavePoint", "pm swp":
+	case "pm storeSpawnPoint", "pm ssp":
+		if reMatchMap["steamid"] != "" {
+			tempPlayer := playerMap[reMatchMap["steamid"]]
+			fmt.Fprintf(serverConn, "pm %s \"Point Added\"\n", tempPlayer.name)
+			spawnPoints = append(spawnPoints, Point{tempPlayer.x, tempPlayer.y, tempPlayer.z})
+			fmt.Printf("SPAWN POINTS: \n%v\n\n", spawnPoints)
+		}
+	case "pm resetSpawnPoints", "pm rsp":
+		if reMatchMap["steamid"] != "" {
+			tempPlayer := playerMap[reMatchMap["steamid"]]
+			fmt.Fprintf(serverConn, "pm %s \"Spawn Points Reset\"\n", tempPlayer.name)
+			spawnPoints = nil
+			fmt.Printf("SPAWN POINTS: \n%v\n\n", spawnPoints)
+		}
+	case "pm spawnTest", "pm st":
+		fmt.Printf("SPAWN TEST\n")
+		if reMatchMap["steamid"] != "" {
+			tempPlayer := playerMap[reMatchMap["steamid"]]
+			fmt.Fprintf(serverConn, "pm %s \"Spawning zombies!\"\n", tempPlayer.name)
+
+			//Store current position
+			tempX := tempPlayer.x
+			tempY := tempPlayer.y
+			tempZ := tempPlayer.z
+
+			for _, tempPoint := range spawnPoints {
+				fmt.Fprintf(serverConn, "tele %s %d %d %d\n", tempPlayer.id, int(tempPoint.x), int(tempPoint.y)+5, int(tempPoint.z))
+				//time.Sleep(time.Second/2)
+				fmt.Fprintf(serverConn, "se %s 17\n", tempPlayer.id)
+			}
+
+			//Restore position
+			fmt.Fprintf(serverConn, "tele %s %d %d %d\n", tempPlayer.id, int(tempX), int(tempY), int(tempZ))
+		}
+	case "pm mainBaseHorde", "pm mbh":
+		fmt.Printf("Main Base Horde On\n")
+		mainBaseHorde = true
+	case "pm stopMainBaseHorde", "pm smbh":
+		fmt.Printf("Main Base Horde Off\n")
+		mainBaseHorde = false
 	}
 }
 
 func tick_trigger(reMatchMap map[string]string) {
 	fmt.Printf("TICK!\n")
+	milliseconds, _ = strconv.ParseInt(reMatchMap["milliseconds"], 10, 64)
+	zombieNum, _ = strconv.ParseInt(reMatchMap["zombienum"], 10, 64)
 }
 
 func player_trigger(reMatchMap map[string]string) {
 	//fmt.Printf("Player Here!\n\n%v\n\n", reMatchMap)
+	//Update player information
+	playerId := reMatchMap["playerid"]
+	tempPlayer := playerMap[playerId]
+	if tempPlayer == nil {
+		tempPlayer = new(Player)
+		playerMap[playerId] = tempPlayer
+		playerMap[reMatchMap["playername"]] = tempPlayer
+		playerMap[reMatchMap["playersteamid"]] = tempPlayer
+
+	}
+
+	//Update info
+	tempPlayer.id = reMatchMap["playerid"]
+	tempPlayer.name = reMatchMap["playername"]
+	tempPlayer.steamId = reMatchMap["playersteamid"]
+	tempPlayer.ip = reMatchMap["playerip"]
+	tempPlayer.ping, _ = strconv.ParseUint(reMatchMap["playerping"], 10, 32)
+	tempPlayer.x, _ = strconv.ParseFloat(reMatchMap["playerx"], 64)
+	tempPlayer.y, _ = strconv.ParseFloat(reMatchMap["playery"], 64)
+	tempPlayer.z, _ = strconv.ParseFloat(reMatchMap["playerz"], 64)
+	tempPlayer.u, _ = strconv.ParseFloat(reMatchMap["playeru"], 64)
+	tempPlayer.v, _ = strconv.ParseFloat(reMatchMap["playerv"], 64)
+	tempPlayer.w, _ = strconv.ParseFloat(reMatchMap["playerw"], 64)
+	tempPlayer.remote, _ = strconv.ParseBool(reMatchMap["playerremote"])
+	tempPlayer.health, _ = strconv.ParseUint(reMatchMap["playerhealth"], 10, 32)
+	tempPlayer.deaths, _ = strconv.ParseUint(reMatchMap["playerdeaths"], 10, 32)
+	tempPlayer.zKills, _ = strconv.ParseUint(reMatchMap["playerzkills"], 10, 32)
+	tempPlayer.pKills, _ = strconv.ParseUint(reMatchMap["playerpkills"], 10, 32)
+	tempPlayer.score, _ = strconv.ParseUint(reMatchMap["playerscore"], 10, 32)
+	tempPlayer.level, _ = strconv.ParseUint(reMatchMap["playerlevel"], 10, 32)
+	tempPlayer.lastUpdate = milliseconds
+	tempPlayer.online = true
+}
+
+func updatePlayerInfo_thread() {
+	//Periodically sends listplayers command, to update player information
+	for {
+		fmt.Fprintf(serverConn, "%s\n", "lp")
+		time.Sleep(time.Second*1)
+
+		//TEMP DEBUG
+		/*
+		fmt.Printf("\n\n\n%v\n", playerMap)
+		for _, tempPlayer := range playerMap {
+			fmt.Printf("%v\n", tempPlayer)
+		}
+		*/
+	}
+}
+
+func mainBaseHorde_thread() {
+	for {
+		if mainBaseHorde {
+			if zombieNum < 80 {
+				//SPAWNZ
+				//Record old location
+				tempX := int(playerMap["Scienthsine"].x)
+				tempY := int(playerMap["Scienthsine"].y)
+				tempZ := int(playerMap["Scienthsine"].z)
+
+				centerX := 1766
+				centerY := 70
+				centerZ := 2216
+
+				for i := 0; i<20; i++ {
+					randAngle := rand.Float64()*math.Pi
+					randDist := 50.0+rand.Float64()*50.0
+					randX := centerX+int(math.Cos(randAngle)*randDist)
+					randZ := centerZ+int(math.Sin(randAngle)*randDist)
+					randZombie := rand.Intn(len(zombies)-1)
+					fmt.Fprintf(serverConn, "tele %s %d %d %d\n", "Scienthsine", randX, centerY, randZ)
+					fmt.Fprintf(serverConn, "se %s %d\n", "Scienthsine", zombies[randZombie])
+				}
+
+				//Restore
+				fmt.Fprintf(serverConn, "tele %s %d %d %d\n", "Scienthsine", tempX, tempY, tempZ)
+			}
+			time.Sleep(time.Minute/4)
+		}
+		time.Sleep(time.Second)
+	}
 }
